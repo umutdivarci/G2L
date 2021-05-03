d98e19242f4578b53d1a9f64210082bcc14eef18
KentyMcDclr8 <66900555+KentyMcDclr8@users.noreply.github.com>
2021-05-03 15:19:54 +0300

    Update DietPlan.java

--- src_2/DietPlan.java
+++ src_2/DietPlan.java
@@ -7,7 +7,7 @@ public class DietPlan {
     final String[] lunchSuggestion = {"Baked/Roasted chicken breast, a large serving of garden salad, a glass of water", "1 slice pizza with meat, a side salad, a glass of juice", "Macaroni and cheese: 1 cup cooked pasta with 2 ounces cheese, 1 cup cooked vegetable, 8 ounces juice"};
     final String[] dinnerSuggestion = {"One cup steamed broccoli, one cup brown rice, one portion of seafood", "4 ounce grilled salmon, 1 cup brown rice, 1 cup cooked vegetable, 12 ounce water", "1 cup turkey chili, 2 ounce cornbread, 8 ounces juice"};
 
-    //instane variables
+    //instance variables
     double targetCaloriesPerDay;
 
     //constructor
@@ -22,5 +22,41 @@ public class DietPlan {
     
     public void setTargetCaloriesPerDay (double newTargetCaloriesPerDay) {
         targetCaloriesPerDay = newTargetCaloriesPerDay;
-    }       
+    }
+    
+    public String getBreakfastSuggestion() {
+        if (targetCaloriesPerDay <= 1500) {
+            return breakfastSuggestion[0];
+        }
+        else if (targetCaloriesPerDay > 1500 && targetCaloriesPerDay < 2000) {
+            return breakfastSuggestion[1];
+        }
+        else {
+            return breakfastSuggestion[2];
+        }
+    }
+    
+    public String getLunchSuggestion() {
+        if (targetCaloriesPerDay <= 1500) {
+            return lunchSuggestion[0];
+        }
+        else if (targetCaloriesPerDay > 1500 && targetCaloriesPerDay < 2000) {
+            return lunchSuggestion[1];
+        }
+        else {
+            return lunchSuggestion[2];
+        }
+    }
+    
+    public String getDinnerSuggestion() {
+        if (targetCaloriesPerDay <= 1500) {
+            return dinnerSuggestion[0];
+        }
+        else if (targetCaloriesPerDay > 1500 && targetCaloriesPerDay < 2000) {
+            return dinnerSuggestion[1];
+        }
+        else {
+            return dinnerSuggestion[2];
+        }
+    }
 }
